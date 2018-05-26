f40b4f5c61b2f610a71d3679bbcd3d00f329b4e4
sharon-launchcode <34315236+sharon-launchcode@users.noreply.github.com>
2018-05-26 09:36:23 -0400

    Update test.js

--- test.js
+++ test.js
@@ -1,4 +1,6 @@
 // event listener ... listen to form submit for initial intake 
 // to do that, must first grab the form
 // get the id
-document.getElementById('contactForm');
+// document.getElementById('contactForm');
+// then add event listener (addEventListener), adding an event (submit )and a function (submit form)
+document.getElementByID('contactForm').addEventListener('submit', 'submitForm');
