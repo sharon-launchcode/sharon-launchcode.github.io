7391583217c22cea5b98bc11641a051bc06cf306
sharon-launchcode <34315236+sharon-launchcode@users.noreply.github.com>
2018-05-25 20:37:25 -0400

    Update intake0.js

--- js/intake0.js
+++ js/intake0.js
@@ -1,1 +1,11 @@
-
+  // Initialize Firebase for Initial Intake Form
+  var config = {
+    apiKey: "AIzaSyAV4773DjZcMNbfQP5Wat0kYl1gHG-lepE",
+    authDomain: "initialintakeform.firebaseapp.com",
+    databaseURL: "https://initialintakeform.firebaseio.com",
+    projectId: "initialintakeform",
+    storageBucket: "initialintakeform.appspot.com",
+    messagingSenderId: "51123204676"
+  };
+  firebase.initializeApp(config);
+  var database = firebase.database();
