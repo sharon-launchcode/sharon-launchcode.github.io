08f30406a0a5623924524c6b9ae68197ebb904f6
sharon-launchcode <34315236+sharon-launchcode@users.noreply.github.com>
2018-05-26 09:58:42 -0400

    Update test.js

--- test.js
+++ test.js
@@ -4,3 +4,10 @@
 // document.getElementById('contactForm');
 // then add event listener (addEventListener), adding an event (submit )and a function (submit form)
 document.getElementByID('contactForm').addEventListener('submit', 'submitForm');
+//create a function called submit form that will take an event (e) ... function submitForm(e) {}
+// and then set up to prevent default behaviour, which will automatically submit to its own page
+// take the event parameter, which is e, and preventDefault ..... e.preventDefault()
+function submitForm(e) {
+  e.preventDefault();
+}
+
