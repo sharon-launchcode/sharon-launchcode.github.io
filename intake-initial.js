60a14ecafc48314051904fefe70b332b7919cec2
sharon-launchcode <34315236+sharon-launchcode@users.noreply.github.com>
2018-05-26 15:16:59 -0400

    Update intake-initial.js

--- intake-initial.js
+++ intake-initial.js
@@ -34,6 +34,20 @@ function getInputVal(id){
   return document.getElementById(id).value;
 }
 
+    // Show alert
+  document.querySelector('.alert').style.display = 'block';
+
+  // Hide alert after 3 seconds
+  setTimeout(function(){
+    document.querySelector('.alert').style.display = 'none';
+  },3000);
+
+  // Clear form
+  document.getElementById('contactForm').reset();
+}
+
+  
+  
 // Save message to firebase
 function saveMessage(name, dateOfBirth, email, phone, state){
   var newMessageRef = messagesRef.push();
