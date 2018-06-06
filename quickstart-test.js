e0c4bd7b8b36b344a8da8752c8a98a2447623d9a
sharon-launchcode <34315236+sharon-launchcode@users.noreply.github.com>
2018-06-06 12:58:13 -0400

    Update quickstart-test.js

--- quickstart-test.js
+++ quickstart-test.js
@@ -1,4 +1,12 @@
-   /**
+  var quickstart = {
+    apiKey: "AIzaSyBlY3OteC7Dn0y0diemxPlg62XgVMqkCHg",
+    authDomain: "signin-fcc07.firebaseapp.com",
+    databaseURL: "https://signin-fcc07.firebaseio.com",
+    projectId: "signin-fcc07",
+    storageBucket: "signin-fcc07.appspot.com",
+    messagingSenderId: "612057419132"
+  };   
+/**
      * Handles the sign in button press.
      */
     function toggleSignIn() {
