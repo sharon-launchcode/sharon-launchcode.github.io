4f849a108184c5d061a75e3854f3312fce6c7e34
sharon-launchcode <34315236+sharon-launchcode@users.noreply.github.com>
2018-06-06 13:55:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
-
-
+  var configapp = {
+    apiKey: "AIzaSyBlY3OteC7Dn0y0diemxPlg62XgVMqkCHg",
+    authDomain: "signin-fcc07.firebaseapp.com",
+    databaseURL: "https://signin-fcc07.firebaseio.com",
+    projectId: "signin-fcc07",
+    storageBucket: "signin-fcc07.appspot.com",
+    messagingSenderId: "612057419132"
+  }; 
+firebase.initializeApp(configapp);
 // Firebase Variables
 var auth = firebase.auth();
 
