fb4f6f2eed2a84d35cfccac073bbe8ab37b3208b
sharon-launchcode <34315236+sharon-launchcode@users.noreply.github.com>
2018-06-06 12:33:01 -0400

    Update config5.js

--- config5.js
+++ config5.js
@@ -2,12 +2,12 @@
   var config5 = {
     apiKey: "AIzaSyBlY3OteC7Dn0y0diemxPlg62XgVMqkCHg",
     authDomain: "signin-fcc07.firebaseapp.com",
-    databaseURL: "https://signin-fcc07.firebaseio.com",
+    //databaseURL: "https://signin-fcc07.firebaseio.com",
     projectId: "signin-fcc07",
     storageBucket: "signin-fcc07.appspot.com",
     messagingSenderId: "612057419132"
   };
-  //const database = firebase.database()
+  const database = firebase.database()
   // From docs: Get a reference to the database service
   // From docs: var database = firebase.database();
   
