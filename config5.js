8dc3282f5977fc5170679564df914f893ad579b3
sharon-launchcode <34315236+sharon-launchcode@users.noreply.github.com>
2018-06-06 11:35:02 -0400

    Update config5.js
    Again moving config file to where there is no duplication of apps

--- config5.js
+++ config5.js
@@ -1,1 +1,9 @@
 
+  var config5 = {
+    apiKey: "AIzaSyBlY3OteC7Dn0y0diemxPlg62XgVMqkCHg",
+    authDomain: "signin-fcc07.firebaseapp.com",
+    databaseURL: "https://signin-fcc07.firebaseio.com",
+    projectId: "signin-fcc07",
+    storageBucket: "signin-fcc07.appspot.com",
+    messagingSenderId: "612057419132"
+  };
