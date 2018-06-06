2d485a2846519d5a7545fd9da117887a57af1155
sharon-launchcode <34315236+sharon-launchcode@users.noreply.github.com>
2018-06-06 12:35:23 -0400

    Update config5.js

--- config5.js
+++ config5.js
@@ -2,7 +2,6 @@
   var config5 = {
     apiKey: "AIzaSyBlY3OteC7Dn0y0diemxPlg62XgVMqkCHg",
     authDomain: "signin-fcc07.firebaseapp.com",
-    //databaseURL: "https://signin-fcc07.firebaseio.com",
     projectId: "signin-fcc07",
     storageBucket: "signin-fcc07.appspot.com",
     messagingSenderId: "612057419132"
