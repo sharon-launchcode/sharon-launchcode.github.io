09f01d66dc06c666305645d5ca16c02e0a5389d4
sharon-launchcode <34315236+sharon-launchcode@users.noreply.github.com>
2018-09-14 18:45:44 -0400

    Update main.js

--- react1/main.js
+++ react1/main.js
@@ -2,7 +2,9 @@
  * Constants
  */
 
-
+var apiKey = “zIxSAzLBj3L96LrJosswrtqi936yHNDK”
+var myDB = “mama-sandbox1”
+var myCollection = “contacts”
 var CONTACT_TEMPLATE = {name: "", email: "", description: "", errors: null}
 
 
